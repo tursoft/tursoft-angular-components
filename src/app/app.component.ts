e705c557b1f718de7aebaf3d1ef6054718297564
Muhammet Turşak <muhammet.tursak@gamyte.com>
2020-09-05 19:48:55 +0300

    ngx-photo-card options support showcase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PhotoInfo } from 'projects/ngx-photo-card/src/lib/models/photoinfo';
+import { PhotoCardOptions } from 'projects/ngx-photo-card/src/lib/models/options';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,24 @@ export class AppComponent {
     { title: 'Javascript', imgSrc: 'http://www.tursoft.net/_files/technologies/original/js.png', imgWidth: '200px' },
     { title: 'Angular', imgSrc: 'http://www.tursoft.net/_files/technologies/original/angular.png', imgWidth: '200px' }
   ];
+
+  options2: PhotoCardOptions =  {
+    borderColor: 'black',
+    borderRadius: '25px',
+    showNumber: false
+  };
+
+  options3: PhotoCardOptions =  {
+    borderColor: 'blue',
+    borderRadius: '0px',
+    showNumber: false
+  };
+
+  selectedPhotoIndex1 = 0;
+  selectedPhotoIndex2 = 1;
+  selectedPhotoIndex3 = 2;
+
+  selectedPhoto1: PhotoInfo = null;
+  selectedPhoto2: PhotoInfo = null;
+  selectedPhoto3: PhotoInfo = null;
 }
