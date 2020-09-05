b8f616f0a68a117ad07dedc8063d1af96a1bb07d
Muhammet Turşak <muhammet.tursak@gamyte.com>
2020-09-05 21:20:50 +0300

    showcase app was refactored

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { PhotoInfo } from 'projects/ngx-photo-card/src/lib/models/photoinfo';
-import { PhotoCardOptions } from 'projects/ngx-photo-card/src/lib/models/options';
 
 @Component({
   selector: 'app-root',
@@ -10,30 +8,4 @@ import { PhotoCardOptions } from 'projects/ngx-photo-card/src/lib/models/options
 export class AppComponent {
   title = 'mycomponents2';
 
-  photos: PhotoInfo[] = [
-    { title: '.NET', imgSrc: 'http://www.tursoft.net/_files/technologies/original/netcore.png', imgWidth: '200px' },
-    { title: 'HTML5', imgSrc: 'http://www.tursoft.net/_files/technologies/original/html5.png', imgWidth: '200px' },
-    { title: 'Javascript', imgSrc: 'http://www.tursoft.net/_files/technologies/original/js.png', imgWidth: '200px' },
-    { title: 'Angular', imgSrc: 'http://www.tursoft.net/_files/technologies/original/angular.png', imgWidth: '200px' }
-  ];
-
-  options2: PhotoCardOptions =  {
-    borderColor: 'black',
-    borderRadius: '25px',
-    showNumber: false
-  };
-
-  options3: PhotoCardOptions =  {
-    borderColor: 'blue',
-    borderRadius: '0px',
-    showNumber: false
-  };
-
-  selectedPhotoIndex1 = 0;
-  selectedPhotoIndex2 = 1;
-  selectedPhotoIndex3 = 2;
-
-  selectedPhoto1: PhotoInfo = null;
-  selectedPhoto2: PhotoInfo = null;
-  selectedPhoto3: PhotoInfo = null;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgxPhotoCardModule } from 'projects/ngx-photo-card/src/lib/ngx-photo-card.module';
+import { TursoftCollapsibleModule } from 'projects/tursoft-collapsible/src/public-api';
+import { NgxPhotoCardComponent } from './components/ngx-photo-card/ngx-photo-card.component';
+import { TursoftCollapsibleComponent } from './components/tursoft-collapsible/tursoft-collapsible.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgxPhotoCardComponent,
+    TursoftCollapsibleComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgxPhotoCardModule
+    NgxPhotoCardModule,
+    TursoftCollapsibleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
