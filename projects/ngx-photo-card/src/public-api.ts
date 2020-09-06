2d58f4741fd8dc8025d012d3e57ef0f598c0ea87
Muhammet Turşak <muhammet.tursak@gamyte.com>
2020-09-06 22:07:19 +0300

    ngx-photo-card - models were exported on public-api

--- projects/ngx-photo-card/src/public-api.ts
+++ projects/ngx-photo-card/src/public-api.ts
@@ -5,3 +5,5 @@
 export * from './lib/services/ngx-photo-card.service/ngx-photo-card.service';
 export * from './lib/components/ngx-photo-card.component/ngx-photo-card.component';
 export * from './lib/ngx-photo-card.module';
+export * from './lib/models/options';
+export * from './lib/models/photoinfo';
